dc6d59a4611cf0199cbc64a93ddf506c73e28d80
Andrey Adriano da Rosa <an060666@intelbras.com.br>
2024-12-10 08:12:28 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"orders-api/application"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	app := application.New()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
+	if err != nil {
+		fmt.Println("failed to start app:", err)
+	}
+}
